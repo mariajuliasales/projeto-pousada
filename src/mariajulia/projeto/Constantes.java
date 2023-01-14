9e6719c8a2bf8927b65599eb8122a6293ba8442f
mariajuliasales <salesmariajulia11@gmail.com>
2023-01-13 21:54:12 -0300

    classe Constante

---
+++ src/mariajulia/projeto/Constantes.java
@@ -0,0 +1,13 @@
+package mariajulia.projeto;
+
+public interface Constantes {
+
+	public static final double SALARIO_FUNC01_HORA = 6.00;
+	public static final double SALARIO_FUNC02_HORA = 7.50;
+	public static final double SALARIO_FUNC03_HORA = 9.00;
+
+	public static final double DIARIA_QUARTO_NORMAL = 89.00;
+	public static final double DIARIA_QUARTO_PLUS = 109.00;
+	public static final double DIARIA_QUARTO_PREMIUM = 120.00;
+
+}
